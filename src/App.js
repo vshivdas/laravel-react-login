e81a153310a16c2b87c72e610536c9cd5073622e
vshivdas <vaibhavshivdas21@gmail.com>
2022-08-10 16:42:43 +0530

    React and laravel task
    React and laravel task

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./App.css";
+import Signup from "./components/signUp/Signup";
+import Signin from "./components/SignIn/Signin";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
+export default class App extends Component {
+  render() {
+    let navLink = (
+      <div className="Tab">
+        <NavLink to="/sign-in" activeClassName="activeLink" className="signIn">
+          Sign In
+        </NavLink>
+        <NavLink exact to="/" activeClassName="activeLink" className="signUp">
+          Sign Up
+        </NavLink>
+      </div>
+    );
+    const login = localStorage.getItem("isLoggedIn");
+
+    return (
+      <div className="App">
+        {login ? (
+          <Router>
+            <Route exact path="/" component={Signup}></Route>
+            <Route path="/sign-in" component={Signin}></Route>
+            <Route path="/home" component={Home}></Route>
+          </Router>
+        ) : (
+          <Router>
+            {navLink}
+            <Route exact path="/" component={Signup}></Route>
+            <Route path="/sign-in" component={Signin}></Route>
+            <Route path="/home" component={Home}></Route>
+          </Router>
+        )}
+      </div>
+    );
+  }
+}
